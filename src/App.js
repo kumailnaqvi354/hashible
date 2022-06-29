85d780816e72ab94a255478008daee36b33ac87c
Nizar Khatri <nizar.ilyas@10pearls.com>
2022-06-29 18:21:28 +0500

    shop page

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import Gallery from './components/Gallery/Gallery';
 import GetStarted from './components/Get Started/GetStarted';
 import Header from './components/Navigation/Header/Header';
 import Roadmap from './components/Roadmap/Roadmap';
+import Shop from './components/Shop/Shop';
 import Team from './components/Team/Team';
 
 function App() {
   return (
     <div className="main-wrapper">
       <Header />
-      <GetStarted />
+      {/* <GetStarted />
       <About />
       <Roadmap />
       <Gallery />
       <Team />
-      <FAQ />
+      <FAQ /> */}
+      <Shop/>
       <Footer />
     </div>
   );
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,493 @@
+import React from 'react'
+
+const Shop = () => {
+  function toggleAccordion(event) {
+    debugger;
+    let items;
+    items = document.getElementsByTagName('article');
+    const article_id = event.currentTarget.getAttribute('id');
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].id !== article_id)
+        items[i].className = "collapsed";
+    }
+
+    if (event.currentTarget.className === "selected expanded") {
+      event.currentTarget.className = "collapsed"
+    }
+    else {
+      event.currentTarget.className = "selected expanded"
+    }
+  }
+  return <>
+    <div className="shop-row">
+
+      <aside className="shop-sidebar">
+        <a href="#" className="filter-collection">
+          <img src={require('../../assets/images/filter-collection.svg').default} alt="filter" />
+        </a>
+      </aside>
+
+      <section className="search-market-row">
+        <div className="search-market-field-row">
+          <input placeholder="Search items, collections, and accounts" role="searchbox" type="text" value="" />
+        </div>
+
+        <div className="search-filter">
+          <select name="" id="">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+      </section>
+    </div>
+
+
+    <div className="mobile-filter-row">
+      <a href="#" className="mobile-filter" data-toggle="modal" data-target="#smart-contract">
+        <img src={require('../../assets/images/filter-mobile.svg').default} alt="mobile-filter" /> Filter</a>
+      <a href="#" className="mobile-sort">
+        <img src={require('../../assets/images/sort-mobile.svg').default} alt="sort-mobile" /> Sort</a>
+    </div>
+
+    <div className="divider-shop">
+
+    </div>
+
+    <div className="market-row">
+      <aside className="market-sidebar">
+        <div className="card">
+          <article id="article-1" className="collapsed" onClick={toggleAccordion}>
+            <div className="market-arrow sidebar-content-heading">Status</div>
+            <div className="content">
+              <div className="market-status-row">
+                <div className="market-checkbox-row active">
+                  <div>Buy Now</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>On Auction</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>New</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>Has Offers</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="sidebar-divider">
+              </div>
+            </div>
+          </article>
+        </div>
+
+        <div className="card">
+          <article id="article-2" className="collapsed" onClick={toggleAccordion}>
+            <div className="market-arrow sidebar-content-heading">Collections</div>
+            <div className="content">
+              <div className="market-status-row">
+                <div className="market-checkbox-row active">
+                  <div>Buy Now</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>On Auction</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>New</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>Has Offers</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </div>
+
+        <div className="card">
+          <article id="article-3" className="collapsed" onClick={toggleAccordion}>
+            <div className="market-arrow sidebar-content-heading">Price</div>
+            <div className="content">
+              <div className="market-status-row">
+                <div className="market-checkbox-row active">
+                  <div>Buy Now</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>On Auction</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>New</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>Has Offers</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </div>
+
+        <div className="card">
+          <article id="article-4" className="collapsed" onClick={toggleAccordion}>
+            <div className="market-arrow sidebar-content-heading">Item Quantity</div>
+            <div className="content">
+              <div className="market-status-row">
+                <div className="market-checkbox-row active">
+                  <div>Buy Now</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>On Auction</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>New</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+              <div className="market-status-row">
+                <div className="market-checkbox-row">
+                  <div>Has Offers</div>
+                  <div><input type="checkbox" /></div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </div>
+
+      </aside>
+      <section className="market-section">
+        <div className="search-results-number">
+          165,256,156 Results
+        </div>
+
+        <div className="filter-btns-action-row">
+          <button className="buy-now">Buy now <img src={require('../../assets/images/close-btn.svg').default} alt='close-btn' /></button>
+          <button className="clear-all">Clear all </button>
+        </div>
+
+        <div className="nft-results-container">
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+            </div>
+          </div>
+
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+          <div className="nft-artwork-result">
+            <div className="nft-sale-img">
+              <img src={require('../../assets/images/117gallery.png')} alt='117-gallery' />
+            </div>
+
+            <div className="nft-artwork-details">
+              <div className="nft-sale-title">
+                Cyber #3968
+              </div>
+
+              <div className="nft-sale-title-second">
+                Weed Cats Family #375
+              </div>
+
+              <div className="nft-sale-market-place">
+                <div>Marketplace</div>
+                <div> <img src={require('../../assets/images/opensea.svg').default} alt='opensea' /> </div>
+              </div>
+
+              <div className="buy-art">
+                Buy Now
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </div>
+  </>
+}
+
+export default Shop
